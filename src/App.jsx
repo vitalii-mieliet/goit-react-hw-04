1a90ac7e0ad02632953cf50101c87b312f9e3fbd
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-07 23:19:05 +0300

    feat: add unsplach API module

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,22 @@
+import { useEffect, useState } from "react";
+import { fetchData } from "./service/unsplashAPI";
 import "./App.css";
 
 function App() {
+  const [query, setQuery] = useState("cats");
+  const [page, setPage] = useState("1");
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    if (!query) return;
+    const fetchImages = async () => {
+      const images = await fetchData(query, page);
+      setImages(images);
+    };
+    fetchImages();
+  }, [query, page]);
+
+  console.log(images);
   return (
     <>
       <h1>This website is under development</h1>
---
+++ src/service/unsplashAPI.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+const unsplashAPI = axios.create({
+  baseURL: "https://api.unsplash.com/",
+  headers: {
+    Authorization: "Client-ID ive54yF3GmSvrYCTdGOz4YvQLscs49Fm6BGsa0_VWIE",
+  },
+});
+
+export async function fetchData(query, page) {
+  const params = {
+    query,
+    page,
+  };
+
+  const { data } = await unsplashAPI.get(`photos/`, {
+    params,
+  });
+  return data;
+}
+
+// const ACCESS_KEY = "ive54yF3GmSvrYCTdGOz4YvQLscs49Fm6BGsa0_VWIE";
+// const params = {
+//   query: "office",
+//   page: 1,
+// };
+
+// export async function fetchImages() {
+//   const response = await axios(
+//     `https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`,
+//     {
+//       params,
+//     }
+//   );
+//   console.log(response);
+// }

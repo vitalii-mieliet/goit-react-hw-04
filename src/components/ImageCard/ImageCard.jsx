d8cce388c06cca12d5254c3c2196033a91923c14
Vitalii Mieliet <vitalii.mieliet@gmail.com>
2025-06-10 04:30:52 +0300

    feat(image-card): display author name with link to profile and optional portfolio icon

--- src/components/ImageCard/ImageCard.jsx
+++ src/components/ImageCard/ImageCard.jsx
@@ -1,6 +1,11 @@
+import { FaRegHeart } from "react-icons/fa6";
+import { FiExternalLink } from "react-icons/fi";
 import styles from "./ImageCard.module.css";
 
-const ImageCard = ({ image: { urls, alt_description }, openModal }) => {
+const ImageCard = ({
+  image: { urls, alt_description, likes, user },
+  openModal,
+}) => {
   return (
     <div className={styles.card}>
       <img
@@ -10,6 +15,36 @@ const ImageCard = ({ image: { urls, alt_description }, openModal }) => {
         loading="lazy"
         onClick={() => openModal({ src: urls.full, alt: alt_description })}
       />
+      <div className={styles.overlay}>
+        <div className={styles.left}>
+          <a
+            href={user.links.html}
+            target="_blank"
+            rel="noreferrer"
+            className={styles.username}
+            title="View Unsplash profile"
+          >
+            @{user.username}
+          </a>
+
+          {user.social?.portfolio_url && (
+            <a
+              href={user.social.portfolio_url}
+              target="_blank"
+              rel="noreferrer"
+              className={styles.link}
+              title="Open personal website"
+            >
+              <FiExternalLink />
+            </a>
+          )}
+        </div>
+
+        <div className={styles.likes}>
+          <FaRegHeart />
+          <span>{likes}</span>
+        </div>
+      </div>
     </div>
   );
 };
